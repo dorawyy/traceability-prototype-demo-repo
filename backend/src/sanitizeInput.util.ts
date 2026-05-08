ebf7e84bdde6a50c693649fa186d235df8e73b70
Andrew Wang <awang60@student.ubc.ca>
2026-05-08 01:02:39 -0700

    test change

--- backend/src/sanitizeInput.util.ts
+++ backend/src/sanitizeInput.util.ts
@@ -2,6 +2,8 @@ export const sanitizeArgs = (args: unknown[]): unknown[] => {
   return args.map(arg => sanitizeInput(String(arg)));
 };
 
+// arbitrary change for testing
+
 export const sanitizeInput = (input: string): string => {
   if (/[\r\n]/.test(input)) {
     throw new Error('CRLF injection attempt detected');
